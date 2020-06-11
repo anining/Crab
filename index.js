36e174daf61a5f26b63e2e8210ebfb062fdd6406
lyk@prismslight.com <530366891@qq.com>
2020-06-12 01:35:16 +0800

    禁止字体因为系统的缩放而缩放

--- index.js
+++ index.js
@@ -3,8 +3,14 @@
  */
 
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import { AppRegistry, Text, TextInput } from 'react-native';
 import AppStackNavigator from './javaScript/navigation/AppStackNavigator';
-import {name as appName} from './app.json';
-
+import { name as appName } from './app.json';
+// 禁止字体因为系统的缩放而缩放
+TextInput.defaultProps = Object.assign({}, TextInput.defaultProps, {
+    allowFontScaling: false,
+}); // 新版RN使用该方法替代
+Text.defaultProps = Object.assign({}, Text.defaultProps, {
+    allowFontScaling: false,
+});
 AppRegistry.registerComponent(appName, () => AppStackNavigator);
