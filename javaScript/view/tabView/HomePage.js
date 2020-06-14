021f5591c675e028dd94df4535f08f06ee497710
lyk@prismslight.com <530366891@qq.com>
2020-06-14 19:07:07 +0800

    创建动画

--- javaScript/view/tabView/HomePage.js
+++ javaScript/view/tabView/HomePage.js
@@ -4,6 +4,7 @@ import { css } from '../../assets/style/css';
 import Android from '../../components/Android';
 import LottieView from 'lottie-react-native';
 import data from '../../lottie/data';
+import data1 from '../../lottie/data1';
 
 export default function HomePage () {
     try {
@@ -26,9 +27,17 @@ export default function HomePage () {
     return (
         <SafeAreaView style={css.safeAreaView}>
             <LottieView
-                style={{ width: '100%', height: 'auto', backgroundColor: 'red' }}
+                style={{ width: '100%', height: 'auto', backgroundColor: '#fff' }}
                 imageAssetsFolder={'lottie'}
                 source={data}
+                loop={true}
+                autoPlay={true}
+                speed={1}
+            />
+            <LottieView
+                style={{ width: '100%', height: 'auto', backgroundColor: '#fff' }}
+                imageAssetsFolder={'lottie1'}
+                source={data1}
                 loop={true}
                 autoPlay={true}
                 speed={1}
