14e96b995535b524bbac8b1af3c88d1adee514eb
lyk@prismslight.com <530366891@qq.com>
2020-06-14 19:46:08 +0800

    组建动画 参考文档https://github.com/oblador/react-native-animatable

--- javaScript/assets/style/css.js
+++ javaScript/assets/style/css.js
@@ -1,8 +1,30 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export const css = StyleSheet.create({
-  safeAreaView: {
-    flex: 1,
-    backgroundColor: '#f3f3f3',//主题颜色
-  },
+    auto: {
+        bottom: 0,
+        left: 0,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        right: 0,
+        top: 0,
+    },
+    flex: {
+        alignContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        justifyContent: 'center',
+    },
+    pa: {
+        position: 'absolute',
+        zIndex: 10
+    },
+    pr: {
+        position: 'relative'
+    },
+    safeAreaView: {
+        backgroundColor: '#f3f3f3',
+        flex: 1, // 主题颜色
+    }
 });
--- javaScript/view/tabView/HomePage.js
+++ javaScript/view/tabView/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { SafeAreaView, Text, DeviceEventEmitter, BackHandler, NativeModules } from 'react-native';
+import { SafeAreaView, View, Text, BackHandler, NativeModules } from 'react-native';
 import { css } from '../../assets/style/css';
 import Android from '../../components/Android';
 import LottieView from 'lottie-react-native';
 import data from '../../lottie/data';
 import data1 from '../../lottie/data1';
+import * as Animatable from 'react-native-animatable';
 
 export default function HomePage () {
     try {
@@ -24,6 +25,20 @@ export default function HomePage () {
     } catch (e) {
         console.log(e);
     }
+    const zoomOut = {
+        0: {
+            opacity: 1,
+            scale: 1,
+        },
+        0.5: {
+            opacity: 1,
+            scale: 0.3,
+        },
+        1: {
+            opacity: 0,
+            scale: 0,
+        },
+    };
     return (
         <SafeAreaView style={css.safeAreaView}>
             <LottieView
@@ -34,14 +49,32 @@ export default function HomePage () {
                 autoPlay={true}
                 speed={1}
             />
-            <LottieView
-                style={{ width: '100%', height: 'auto', backgroundColor: '#5d6fff' }}
-                imageAssetsFolder={'lottie1'}
-                source={data1}
-                loop={true}
-                autoPlay={true}
-                speed={1}
-            />
+            <View style={[css.flex, css.pr]}>
+                <LottieView
+                    style={{ width: '100%', height: 'auto', backgroundColor: '#5d6fff' }}
+                    imageAssetsFolder={'lottie1'}
+                    source={data1}
+                    loop={true}
+                    autoPlay={true}
+                    speed={1}
+                />
+                <View style={[css.pa, {
+                    top: 20,
+                }]}>
+                    {/* 参考文档https://github.com/oblador/react-native-animatable */}
+                    <Animatable.Text useNativeDriver={true} iterationCount={50} animation="tada" style={{
+                        width: 100,
+                        height: 40,
+                        color: '#fff',
+                        fontSize: 12,
+                        backgroundColor: '#dc4a1d',
+                        textAlign: 'center',
+                        lineHeight: 40,
+                        borderRadius: 20,
+                        overflow: 'hidden',
+                    }}>买买买！</Animatable.Text>
+                </View>
+            </View>
         </SafeAreaView>
     );
 }
