216d5797b44db2ed01771f7f6909639f5483af67
tanjinping <tanjinping9@gmail.com>
2020-06-16 21:01:14 +0800

    完善TaskDetailPage页面

--- javaScript/view/tabView/SharePage.js
+++ javaScript/view/tabView/SharePage.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-import {css} from '../../assets/style/css';
+import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
+import { css } from '../../assets/style/css';
+import { N } from '../../utils/router';
 
-export default function SharePage() {
-  return (
-    <SafeAreaView style={css.safeAreaView}>
-      <Text>SharePage</Text>
-    </SafeAreaView>
-  );
+export default function SharePage () {
+    return (
+        <SafeAreaView style={css.safeAreaView}>
+            <TouchableOpacity onPress={() => {
+                N.navigate('TaskDetailPage');
+            }}>
+                <Text style={{ lineHeight: 30, textAlign: 'center' }}>TaskDetailPage</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
 }
