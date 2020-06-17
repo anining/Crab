2462458c9658ec794912d83e76906ddefd5982ee
tanjinping <tanjinping9@gmail.com>
2020-06-17 13:40:41 +0800

    新增UserAgreementPage、PrivacyPolicyPage、GlossaryPage、ActivityCenterPage页面

---
+++ javaScript/view/userView/ActivityCenterPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+import { css } from '../../assets/style/css';
+
+export default function ActivityCenterPage () {
+    return (
+        <SafeAreaView style={css.safeAreaView}>
+            <Text>ActivityCenterPage</Text>
+        </SafeAreaView>
+    );
+}
---
+++ javaScript/view/userView/GlossaryPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+import { css } from '../../assets/style/css';
+
+export default function GlossaryPage () {
+    return (
+        <SafeAreaView style={css.safeAreaView}>
+            <Text>GlossaryPage</Text>
+        </SafeAreaView>
+    );
+}
