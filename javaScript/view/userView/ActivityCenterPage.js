eaa1816be0db0ad4483df20c05adbce4454b2955
tanjinping <tanjinping9@gmail.com>
2020-06-20 17:30:18 +0800

    完善ActivityCenterPage数据

--- javaScript/view/userView/ActivityCenterPage.js
+++ javaScript/view/userView/ActivityCenterPage.js
@@ -1,22 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { SafeAreaView, Image, Text, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { css } from '../../assets/style/css';
 import ListGeneral from '../../components/ListGeneral';
-import activity1 from '../../assets/icon/activity/activity1.png';
-import activity2 from '../../assets/icon/activity/activity2.png';
 import { N } from '../../utils/router';
 import { activity } from '../../utils/api';
+import { transformTime } from '../../utils/util';
 
 const itemHeight = 170;
 const itemMarginTop = 10;
-const TYPE_DATA = [activity2, activity2, activity1];
 
 export default function ActivityCenterPage () {
-    useEffect(() => {
-        activity().then(r => {
-            console.log(r);
-        });
-    }, []);
     return (
         <SafeAreaView style={css.safeAreaView}>
             <View style={{ paddingLeft: 15, paddingRight: 15, flex: 1, backgroundColor: '#F8F8F8' }}>
@@ -24,53 +17,32 @@ export default function ActivityCenterPage () {
                     itemHeight={itemHeight}
                     itemMarginTop={itemMarginTop}
                     getList={async (page, num, callback) => {
-                        // eslint-disable-next-line standard/no-callback-literal
-                        callback([
-                            {
-                                type: 1,
-                                path: '',
-                                label: '天天领现金',
-                                detail: '天天送红包！提交并且通过指定数量的任务就可以开红包，最低每天天送红包！提交并且通过指定数量的任务就……',
-                                deadline: '长期有效',
-                                tag: '即将结束'
-                            },
-                            {
-                                type: 2,
-                                path: '',
-                                label: '天天领现金',
-                                detail: '天天送红包！提交并且通过指定数量的任务就可以开红包，最低每天天送红包！提交并且通过指定数量的任务就……',
-                                deadline: '长期有效',
-                                tag: '即将结束'
-                            },
-                            {
-                                type: 1,
-                                path: '',
-                                label: '天天领现金',
-                                detail: '天天送红包！提交并且通过指定数量的任务就可以开红包，最低每天天送红包！提交并且通过指定数量的任务就……',
-                                deadline: '长期有效',
-                                tag: '即将结束'
-                            },
-                        ]);
+                        activity(page, num).then(r => {
+                            if (!r.error) {
+                                callback(r.data);
+                            }
+                        });
                     }}
                     renderItem={item => {
+                        const { activity_id, des, path, icon, start_datetime, title } = item;
                         return (
                             <>
-                                <View style={styles.itemView} key={item.type + item.detail}>
+                                <View style={styles.itemView} key={activity_id}>
                                     <View style={[css.flexRCSB, styles.item]}>
                                         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                            <Image source={TYPE_DATA[item.type]} style={styles.image} />
-                                            <Text numberOfLines={1} style={{ fontSize: 14, fontWeight: '500', color: '#353535', maxWidth: 130 }}>{item.label}</Text>
+                                            <Image source={{ uri: icon }} style={styles.image} />
+                                            <Text numberOfLines={1} style={{ fontSize: 14, fontWeight: '500', color: '#353535', maxWidth: 130 }}>{title}</Text>
                                         </View>
                                         <TouchableOpacity activeOpacity={1} onPress={() => {
-                                            // N.navigate(item.path);
+                                            N.navigate(path);
                                         }}>
                                             <Text numberOfLines={1} style={{ fontSize: 12, fontWeight: '500', color: '#353535' }}>查看详情 》</Text>
                                         </TouchableOpacity>
                                     </View>
-                                    <Text numberOfLines={2} style={styles.text}>封停期限：{item.detail}</Text>
+                                    <Text numberOfLines={2} style={styles.text}>{des}</Text>
                                     <View style={[css.flexRCSB, styles.item, { borderTopWidth: 1, borderTopColor: '#EDEDED', marginTop: 20 }]}>
-                                        <Text numberOfLines={1} style={{ maxWidth: 180, fontSize: 14, color: '#353535' }}>活动时间：{item.deadline}</Text>
-                                        <Text numberOfLines={1} style={{ maxWidth: 180, fontSize: 14, color: '#FA6400', fontWeight: '500' }}>{item.tag}</Text>
+                                        <Text numberOfLines={1} style={{ maxWidth: 250, fontSize: 14, color: '#353535' }}>活动时间：{transformTime(start_datetime)}</Text>
+                                        <Text numberOfLines={1} style={{ maxWidth: 180, fontSize: 14, color: '#FA6400', fontWeight: '500' }}>即将结束</Text>
                                     </View>
                                 </View>
                             </>
@@ -106,5 +78,5 @@ const styles = StyleSheet.create({
         lineHeight: 25,
         paddingLeft: 15,
         paddingRight: 15
-    },
+    }
 });
