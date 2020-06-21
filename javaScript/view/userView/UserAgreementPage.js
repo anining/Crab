0c7b18bd81275a26d212c21c0292cbf2c52feb22
tanjinping <tanjinping9@gmail.com>
2020-06-21 14:01:29 +0800

    完善UserAgreementPage数据

--- javaScript/view/userView/UserAgreementPage.js
+++ javaScript/view/userView/UserAgreementPage.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Dimensions } from 'react-native';
+import HTML from 'react-native-render-html';
 import { css } from '../../assets/style/css';
+import { getter } from '../../utils/store';
 
+const { width } = Dimensions.get('window');
 export default function UserAgreementPage () {
+    const { agreement } = getter(['app.agreement']);
+
     return (
         <SafeAreaView style={[css.safeAreaView, css.RichText]}>
-            <Text>UserAgreementPage</Text>
+            <HTML html={agreement.get()} imagesMaxWidth={width} tagsStyles={{ p: { lineHeight: 24 } }}/>
         </SafeAreaView>
     );
 }
