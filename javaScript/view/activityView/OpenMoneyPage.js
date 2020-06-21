fb31744b7fbf9be8de694ee2408f5bde3dcd1d59
lyk@prismslight.com <530366891@qq.com>
2020-06-21 11:22:34 +0800

    红包页面

--- javaScript/view/activityView/OpenMoneyPage.js
+++ javaScript/view/activityView/OpenMoneyPage.js
@@ -8,13 +8,15 @@ import {
     StyleSheet,
     Text,
     TouchableOpacity,
+    View
 } from 'react-native';
 import { css } from '../../assets/style/css';
 import activity7 from '../../assets/icon/activity/activity7.png';
+import activity8 from '../../assets/icon/activity/activity8.png';
 import Header from '../../components/Header';
 import header3 from '../../assets/icon/header/header3.png';
 import ImageAuto from '../../components/ImageAuto';
-import activity6 from '../../assets/icon/activity/activity6.png';
+import activity14 from '../../assets/icon/activity/activity14.png';
 import { activityDetail } from '../../utils/api';
 import { N } from '../../utils/router';
 const { height, width } = Dimensions.get('window');
@@ -44,6 +46,31 @@ export default class OpenMoneyPage extends Component {
                     this.setState({
                         receivedStatus: 1
                     });
+                    DeviceEventEmitter.emit('showPop', {
+                        dom: <View style={[css.pr]}>
+                            <TouchableOpacity activeOpacity={1} style={[styles.redInnerWrap, css.pa, css.flex, css.fw]} onPress={() => {
+                                this.setState({
+                                    receivedStatus: 2
+                                }, () => {
+                                    DeviceEventEmitter.emit('hidePop');
+                                });
+                            }}>
+                                <ImageAuto style={{
+                                    width: 48,
+                                    borderRadius: 20,
+                                }} source={activity8}/>
+                                <Text style={styles.redNameText}>运营商送你一个红包</Text>
+                                <Text style={styles.redTipsText}>现在打开</Text>
+                                <Text style={styles.redTipsText}>最低20元现金等着你</Text>
+                            </TouchableOpacity>
+                            <ImageAuto width={width * 0.8} source={activity14}/>
+                        </View>,
+                        close: () => {
+                            if (this.state.receivedStatus < 2) {
+                                N.goBack();
+                            }
+                        }
+                    });
                 }
             }
         } catch (e) {
@@ -57,9 +84,11 @@ export default class OpenMoneyPage extends Component {
                 <ImageBackground source={activity7} style={[styles.dmWrap]}>
                     <Header color={'#fff'} label={'天天领现金'} style={{ backgroundColor: 'rgba(0,0,0,0)', borderBottomWidth: 0 }} icon={header3}/>
                     {_if(this.state.receivedStatus === 0, res => {
-                        return <Text style={styles.loadingText}>加载中...</Text>;
+                        return null;
                     }, () => {
+                        return <View>
 
+                        </View>;
                     })}
                 </ImageBackground>
             </ScrollView>
@@ -75,6 +104,33 @@ const styles = StyleSheet.create({
         color: '#fff',
         fontSize: 15,
         marginTop: width * 0.3,
+        textAlign: 'center',
+        width: '100%'
+    },
+    redInnerWrap: {
+        // backgroundColor: 'red',
+        flex: 1,
+        height: '100%',
+        padding: 20,
+        width: '100%',
+        // eslint-disable-next-line react-native/sort-styles
+        paddingLeft: '13%',
+        paddingBottom: '30%',
+        overflow: 'hidden'
+    },
+    redNameText: {
+        color: '#FDFAB1',
+        fontSize: 14,
+        lineHeight: 40,
+        marginTop: 10,
+        textAlign: 'center',
+        width: '100%'
+    },
+    redTipsText: {
+        color: '#FDFAB1',
+        fontSize: 17,
+        fontWeight: '900',
+        lineHeight: 40,
         textAlign: 'center',
         width: '100%'
     }
