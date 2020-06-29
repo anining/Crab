769c4a710a2c5bd18dca22003a2d650dcfd644cf
tanjinping <tanjinping9@gmail.com>
2020-06-29 14:20:00 +0800

    完善PupilInfoPage师徒信息页面

--- javaScript/view/shareView/PupilInfoPage.js
+++ javaScript/view/shareView/PupilInfoPage.js
@@ -1,133 +1,188 @@
-import React, { Component } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View, ImageBackground, Dimensions, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, ImageBackground, Dimensions, TouchableOpacity, ScrollView, Image } from 'react-native';
 import { css } from '../../assets/style/css';
 import Header from '../../components/Header';
 import LinearGradient from 'react-native-linear-gradient';
-import Shadow from '../../components/Shadow';
+import Clipboard from '@react-native-community/clipboard';
 import { N } from '../../utils/router';
 import pupil1 from '../../assets/icon/pupil/pupil1.png';
 import pupil2 from '../../assets/icon/pupil/pupil2.png';
 import pupil3 from '../../assets/icon/pupil/pupil3.png';
 import pupil4 from '../../assets/icon/pupil/pupil4.png';
 import pupil5 from '../../assets/icon/pupil/pupil5.png';
-import pupil6 from '../../assets/icon/pupil/pupil6.png';
 import pupil7 from '../../assets/icon/pupil/pupil7.png';
 import pupil8 from '../../assets/icon/pupil/pupil8.png';
-import pupil9 from '../../assets/icon/pupil/pupil9.png';
 import pupil10 from '../../assets/icon/pupil/pupil10.png';
-import ListGeneral from '../../components/ListGeneral';
 import ImageAuto from '../../components/ImageAuto';
-import share8 from '../../assets/icon/share/share8.png';
 import { childDetail } from '../../utils/api';
-import { _tc } from '../../utils/util';
-const { height, width } = Dimensions.get('window');
-export default class PupilInfoPage extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {};
-    }
+import { transformTime } from '../../utils/util';
+import toast from '../../utils/toast';
+const { width } = Dimensions.get('window');
+const itemHeight = 135;
+const itemMarginTop = 10;
 
-    componentDidMount () {
-        this._childDetail();
-    }
+function PupilInfoPage () {
+    const [children, setChildren] = useState([]);
+    const [parent, setParent] = useState({});
+    const [today, setToday] = useState({});
+    const [yesterday, setYesterday] = useState({});
+    const [total, setTotal] = useState({});
 
-    _childDetail () {
-        _tc(async () => {
-            const ret = await childDetail();
-            console.log(ret, '??');
+    useEffect(() => {
+        _childDetail();
+    }, []);
+
+    function _childDetail () {
+        childDetail().then(r => {
+            if (!r.error) {
+                const { children_list, parent, today, yesterday, total } = r.data;
+                setChildren(children_list);
+                parent && setParent(parent);
+                setToday(today);
+                setYesterday(yesterday);
+                setTotal(total);
+            }
         });
     }
 
-    static _renderShareTitle (title, icon, width) {
-        try {
-            return <View style={[css.flex, css.js, { width: width || '100%' }]}>
-                <ImageAuto source={icon} style={{ width: 20, marginRight: 10 }}/>
-                <Text style={styles.pupTitleText}>{title || '-'}</Text>
-            </View>;
-        } catch (e) {
-            console.log(e);
-            return null;
-        }
-    }
+    const { invite_code = 'xxx', avatar = 'xxx', nickname = 'xxx', qq_group = 'xxx', wx = 'xxx' } = parent;
+    return (
+        <SafeAreaView style={[css.safeAreaView, { backgroundColor: '#f8f8f8' }]}>
+            <Header label={'师徒信息'} onPress={() => { N.navigate('PupilSetPage'); }} headerRight={<Text style={{ color: '#FF5C22' }}>收徒设置</Text>}/>
+            <ScrollView>
+                <View style={[styles.infoHeaderWrap, css.pr]}>
+                    <LinearGradient colors={['#FF9C00', '#FF3E00']} start={{ x: 0, y: 1 }} end={{ x: 1, y: 1 }} style={[styles.infoHeaderBg]} />
+                    <ImageBackground source={pupil8} style={[styles.infoHeader, css.pa]}>
+                        <RenderShareTitle title="我的师傅" icon={pupil5}/>
+                        <View style={[styles.teacherWrap, css.flex, css.sp]}>
+                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                <Image source={{ uri: avatar }} style={{ width: 40, height: 40, borderRadius: 20 }}/>
+                                <Text style={{ color: 'rgba(53,53,53,1)', fontSize: 16, fontWeight: '500', marginLeft: 5 }}>{nickname}</Text>
+                            </View>
+                            <Text style={{ color: '#999', fontSize: 12 }}>他的邀请码：{invite_code} </Text>
+                        </View>
+                        <View style={[css.flex, styles.pupBtnWrap, css.auto, css.sp]}>
+                            <TouchableOpacity onPress={() => {
+                                Clipboard.setString(wx.toString());
+                                toast('复制成功');
+                            }}>
+                                <ImageAuto source={pupil7} width={width * 0.35}/>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => {
+                                Clipboard.setString(qq_group.toString());
+                                toast('复制成功');
+                            }}>
+                                <ImageAuto source={pupil10} width={width * 0.35}/>
+                            </TouchableOpacity>
+                        </View>
+                    </ImageBackground>
+                </View>
+                <RenderData today={today} yesterday={yesterday} total={total}/>
+                <View style={[styles.pupListWrap, css.flex, css.sp]}>
+                    <RenderShareTitle title="徒弟列表" icon={pupil4} width={200}/>
+                    <Text style={styles.pupListTips}>总贡献排序</Text>
+                </View>
+                <RenderChild children={children}/>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
 
-    _renderForm () {
-        const that = this;
-        return <View style={styles.formWrap}>
+function RenderChild ({ children }) {
+    const view = [];
+    children.forEach(item => {
+        const { today_income, total_income, children_num, avatar = 'http://wwww', total_task_num, created_at, nickname, user_id } = item;
+        view.push(
+            <View style={styles.itemContainer} key={user_id}>
+                <View style={[styles.containerT]}>
+                    <View style={{ alignItems: 'center', flexDirection: 'row', height: 55 }}>
+                        <Image source={{ uri: avatar }} style={{ width: 30, height: 30, borderRadius: 15 }}/>
+                        <Text style={{ color: 'rgba(53,53,53,1)', fontWeight: '600', fontSize: 15 }}>{nickname}</Text>
+                    </View>
+                    <Text style={{ color: 'rgba(153,153,153,1)', fontSize: 11 }}>收徒时间：{transformTime(created_at)}</Text>
+                </View>
+                <View style={{ alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', height: 40 }}>
+                    <Text style={{ color: 'rgba(85,85,85,1)' }}>今日贡献：<Text style={{ color: 'rgba(51,51,51,1)' }}>{today_income}</Text></Text>
+                    <Text style={{ color: 'rgba(85,85,85,1)' }}>累计贡献金币：<Text style={{ color: 'rgba(51,51,51,1)' }}>{total_income}</Text></Text>
+                </View>
+                <View style={{ alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', height: 40 }}>
+                    <Text style={{ color: 'rgba(85,85,85,1)' }}>他的徒弟总数：<Text style={{ color: 'rgba(51,51,51,1)' }}>{children_num}</Text></Text>
+                    <Text style={{ color: 'rgba(85,85,85,1)' }}>他的做单总数：<Text style={{ color: 'rgba(51,51,51,1)' }}>{total_task_num}</Text></Text>
+                </View>
+            </View>
+        );
+    });
+    return <View style={{ marginBottom: 15 }}>{view}</View>;
+}
+
+function RenderData ({ today, yesterday, total }) {
+    const array = [
+        {
+            label: '今日收徒数据',
+            icon: pupil1,
+            ...today
+        },
+        {
+            label: '昨日收徒数据',
+            icon: pupil2,
+            ...yesterday
+        },
+        {
+            label: '累计收徒数据',
+            icon: pupil3,
+            ...total
+        },
+    ];
+    const view = [];
+    array.forEach(item => {
+        const { children_income, children_num, disciple_income, disciple_num, label, icon } = item;
+        view.push(
+            <View style={[css.flex, css.fw, styles.pupilItemWrap]}>
+                <RenderShareTitle title={label} icon={icon}/>
+                <RenderForm children_income={children_income} children_num={children_num} disciple_income={disciple_income} disciple_num={disciple_num}/>
+            </View>
+        );
+    });
+    return <>{view}</>;
+}
+
+function RenderShareTitle ({ title, icon, width }) {
+    return (
+        <View style={[css.flex, css.js, { width: width || '100%' }]}>
+            <ImageAuto source={icon} style={{ width: 20, marginRight: 10 }}/>
+            <Text style={styles.pupTitleText}>{title || '-'}</Text>
+        </View>
+    );
+}
+
+function RenderForm ({ children_income, children_num, disciple_income, disciple_num }) {
+    return (
+        <View style={styles.formWrap}>
             <View style={[styles.formHeaderWrap, css.flex]}>
+                <Text style={styles.fhwText}>徒弟人数</Text>
                 <Text style={styles.fhwText}>徒孙人数</Text>
-                <Text style={styles.fhwText}>徒孙人数</Text>
-                <Text style={styles.fhwText}>徒孙人数</Text>
-                <Text style={styles.fhwText}>徒孙人数</Text>
+                <Text style={styles.fhwText}>徒弟贡献</Text>
+                <Text style={styles.fhwText}>徒孙贡献</Text>
             </View>
             <View style={[styles.formLineWrap, css.flex]}>
-                <Text style={styles.fhwLineText}>321</Text>
-                <Text style={styles.fhwLineText}>321</Text>
-                <Text style={styles.fhwLineText}>321</Text>
-                <Text style={[styles.fhwLineText, { borderRightWidth: 0 }]}>321</Text>
+                <Text style={styles.fhwLineText}>{children_num}</Text>
+                <Text style={styles.fhwLineText}>{disciple_num}</Text>
+                <Text style={styles.fhwLineText}>{children_income}</Text>
+                <Text style={[styles.fhwLineText, { borderRightWidth: 0 }]}>{disciple_income}</Text>
             </View>
-        </View>;
-    }
-
-    render () {
-        return <SafeAreaView style={[css.safeAreaView, { backgroundColor: '#f8f8f8' }]}>
-            <Header label={'师徒信息'} onPress={() => {
-                N.navigate('PupilSetPage');
-            }} headerRight={<Text style={{ color: '#FF5C22' }}>收徒设置</Text>}/>
-            <ListGeneral
-                itemHeight={20}
-                itemMarginTop={10}
-                getList={async (page, num, callback) => {
-                    // eslint-disable-next-line standard/no-callback-literal
-                    callback([]);
-                }} // 获取列表的api
-                renderHeader={() => {
-                    return <View>
-                        <View style={[styles.infoHeaderWrap, css.pr]}>
-                            <LinearGradient colors={['#FF9C00', '#FF3E00']} start={{ x: 0, y: 1 }} end={{ x: 1, y: 1 }} style={[styles.infoHeaderBg]} />
-                            <ImageBackground source={pupil8} style={[styles.infoHeader, css.pa]}>
-                                {PupilInfoPage._renderShareTitle('我的师傅', pupil5)}
-                                <View style={[styles.teacherWrap, css.flex, css.sp]}>
-                                    <ImageAuto source={pupil2} width={40}/>
-                                    <Text style={{ color: '#999', fontSize: 12 }}>他的邀请码：A67BHD12 </Text>
-                                </View>
-                                <View style={[css.flex, styles.pupBtnWrap, css.auto, css.sp]}>
-                                    <TouchableOpacity>
-                                        <ImageAuto source={pupil7} width={width * 0.35}/>
-                                    </TouchableOpacity>
-                                    <TouchableOpacity>
-                                        <ImageAuto source={pupil10} width={width * 0.35}/>
-                                    </TouchableOpacity>
-                                </View>
-                            </ImageBackground>
-                        </View>
-                        <View style={[css.flex, css.fw, styles.pupilItemWrap]}>
-                            {PupilInfoPage._renderShareTitle('今日收徒数据', pupil1)}
-                            {this._renderForm()}
-                        </View>
-                        <View style={[css.flex, css.fw, styles.pupilItemWrap]}>
-                            {PupilInfoPage._renderShareTitle('昨日收徒数据', pupil2)}
-                            {this._renderForm()}
-                        </View>
-                        <View style={[css.flex, css.fw, styles.pupilItemWrap]}>
-                            {PupilInfoPage._renderShareTitle('累计收徒数据', pupil3)}
-                            {this._renderForm()}
-                        </View>
-                        <View style={[styles.pupListWrap, css.flex, css.sp]}>
-                            {PupilInfoPage._renderShareTitle('徒弟列表', pupil4, 200)}
-                            <Text style={styles.pupListTips}>总贡献排序</Text>
-                        </View>
-                    </View>;
-                }}
-                renderItem={(item, index) => {
-                    return (<View>
-
-                    </View>);
-                }}
-            />
-        </SafeAreaView>;
-    }
+        </View>
+    );
 }
+
 const styles = StyleSheet.create({
+    containerT: {
+        alignItems: 'center',
+        borderBottomColor: '#FFF2F2F2',
+        borderBottomWidth: 1,
+        flexDirection: 'row',
+        height: 55,
+        justifyContent: 'space-between'
+    },
     fhwLineText: {
         borderRightColor: '#FFE3CF',
         borderRightWidth: 1,
@@ -180,6 +235,17 @@ const styles = StyleSheet.create({
     infoHeaderWrap: {
         backgroundColor: '#f8f8f8',
         height: 210,
+        marginBottom: 15
+    },
+    itemContainer: {
+        backgroundColor: '#FFF',
+        borderRadius: 8,
+        height: itemHeight,
+        marginLeft: 15,
+        marginRight: 15,
+        marginTop: itemMarginTop,
+        paddingLeft: 15,
+        paddingRight: 15
     },
     pupBtnWrap: {
         height: 'auto',
@@ -214,5 +280,7 @@ const styles = StyleSheet.create({
         height: 90,
         paddingHorizontal: 20,
         width: '100%'
-    },
+    }
 });
+
+export default PupilInfoPage;
