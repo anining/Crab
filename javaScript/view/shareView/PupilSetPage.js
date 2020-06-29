e3e0dac438400c9ef7e836398d836cc240907204
tanjinping <tanjinping9@gmail.com>
2020-06-29 19:53:35 +0800

    完善PupilSetPage师父设置页面

--- javaScript/view/shareView/PupilSetPage.js
+++ javaScript/view/shareView/PupilSetPage.js
@@ -1,58 +1,80 @@
-import React, { Component } from 'react';
-import { Dimensions, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Dimensions, SafeAreaView, StyleSheet, TouchableOpacity, ScrollView, Text, Image, TextInput, View } from 'react-native';
 import { css } from '../../assets/style/css';
-import ImageAuto from '../../components/ImageAuto';
 import pupil5 from '../../assets/icon/pupil/pupil5.png';
-const { height, width } = Dimensions.get('window');
-export default class PupilSetPage extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {};
+import Crab from '../../components/Crab';
+import { childrenSetting } from '../../utils/api';
+const { width } = Dimensions.get('window');
+
+function PupilSetPage () {
+    const [qq, setQq] = useState('');
+    const [wx, setWx] = useState('');
+    const [num, setNum] = useState('');
+    const [totalMoney, setTotalMoney] = useState('');
+
+    function save () {
+        childrenSetting(qq, wx, num, totalMoney).then(r => {
+            console.log(r);
+        });
     }
 
-    static _renderShareTitle (title, icon, width) {
-        try {
-            return <View style={[css.flex, css.js, { width: width || '100%' }]}>
-                <ImageAuto source={icon} style={{ width: 20, marginRight: 10 }}/>
-                <Text style={styles.pupTitleText}>{title || '-'}</Text>
-            </View>;
-        } catch (e) {
-            console.log(e);
-            return null;
-        }
-    }
-
-    render () {
-        return <SafeAreaView style={[css.safeAreaView, { backgroundColor: '#f8f8f8' }]}>
-            <Text style={styles.setTips}>我们承诺不会泄露你的个人信息，请放心填写。</Text>
-            <View style={styles.formAllWrap}>
-                {PupilSetPage._renderShareTitle('基本信息', pupil5)}
-                <View style={[css.flex, css.sp, styles.inputWrap]}>
-                    <Text style={[styles.setInputText]} numberOfLines={1}>QQ群号</Text>
-                    <TextInput style={[styles.setInput]} maxLength={30} placeholder={'请输入QQ群号'} placeholderTextColor={'#BCBCBC'} onChangeText={() => {
-
-                    }}/>
+    return (
+        <SafeAreaView style={[css.safeAreaView, { backgroundColor: '#f8f8f8' }]}>
+            <ScrollView>
+                <Text style={styles.setTips}>我们承诺不会泄露你的个人信息，请放心填写。</Text>
+                <View style={styles.formAllWrap}>
+                    <RenderShareTitle title='基本信息' icon={pupil5}/>
+                    <View style={[css.flex, css.sp, styles.inputWrap]}>
+                        <Text style={[styles.setInputText]} numberOfLines={1}>QQ群号</Text>
+                        <TextInput style={[styles.setInput]} maxLength={30} placeholder={'请输入QQ群号'} placeholderTextColor={'#BCBCBC'} onChangeText={e => setQq(e)}/>
+                    </View>
+                    <View style={[css.flex, css.sp, styles.inputWrap]}>
+                        <Text style={[styles.setInputText]} numberOfLines={1}>微信号</Text>
+                        <TextInput style={[styles.setInput]} maxLength={30} placeholder={'请输入QQ群号'} placeholderTextColor={'#BCBCBC'} onChangeText={e => setWx(e)}/>
+                    </View>
+                    <View style={[css.flex, css.sp, styles.inputWrap]}>
+                        <Text style={[styles.setInputText]} numberOfLines={1}>邀请人数</Text>
+                        <TextInput style={[styles.setInput]} maxLength={30} placeholder={'需邀请多少人才展示信息(无要求不填)'} placeholderTextColor={'#BCBCBC'} onChangeText={e => setNum(e)}/>
+                    </View>
+                    <View style={[css.flex, css.sp, styles.inputWrap]}>
+                        <Text style={[styles.setInputText]} numberOfLines={1}>收益总和</Text>
+                        <TextInput style={[styles.setInput]} maxLength={30} placeholder={'需达到多少收益才展示信息(无要求不填)'} placeholderTextColor={'#BCBCBC'} onChangeText={e => setTotalMoney(e)}/>
+                    </View>
                 </View>
-                <View style={[css.flex, css.sp, styles.inputWrap]}>
-                    <Text style={[styles.setInputText]} numberOfLines={1}>微信号</Text>
-                    <TextInput style={[styles.setInput]} maxLength={30} placeholder={'请输入QQ群号'} placeholderTextColor={'#BCBCBC'} onChangeText={() => {
-
-                    }}/>
+                <View style={styles.setInfoAllText}>
+                    <Crab text="设置说明：" paddingLeft={0}/>
+                    <Text style={styles.setLineText}>1.请先填写基本信息，不填展示的信息为空。</Text>
+                    <Text style={styles.setLineText}>2.QQ群勾选徒弟可见，达到要求的徒弟可以一键加群。</Text>
+                    <Text style={styles.setLineText}>3.微信号勾选徒弟可见，达到要求的徒弟可以复制微信并添加微信。</Text>
+                    <Text style={styles.setLineText}>4.勾选并填写邀请人数，徒弟达到邀请人数之后可见基本信息。</Text>
+                    <Text style={styles.setLineText}>5.勾选并填写收益总和，徒弟达到收益总和之后可见基本信息。</Text>
+                    <Text style={styles.setLineText}>6.信息可以随时更换，更换信息之后只影响到之后的徒弟。</Text>
                 </View>
-            </View>
-            <View style={styles.setInfoAllText}>
-                <Text style={styles.setTitle}>设置说明</Text>
-                <Text style={styles.setLineText}>1.请先填写基本信息，不填展示的信息为空。</Text>
-                <Text style={styles.setLineText}>2.QQ群勾选徒弟可见，达到要求的徒弟可以一键加群。</Text>
-                <Text style={styles.setLineText}>3.微信号勾选徒弟可见，达到要求的徒弟可以复制微信并添加微信。</Text>
-                <Text style={styles.setLineText}>4.勾选并填写邀请人数，徒弟达到邀请人数之后可见基本信息。</Text>
-                <Text style={styles.setLineText}>5.勾选并填写收益总和，徒弟达到收益总和之后可见基本信息。</Text>
-                <Text style={styles.setLineText}>6.信息可以随时更换，更换信息之后只影响到之后的徒弟。</Text>
-            </View>
-        </SafeAreaView>;
-    }
+            </ScrollView>
+            <TouchableOpacity onPress={save} style={styles.btn}>
+                <Text style={{ fontSize: 17, color: '#fff' }}>保存设置</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
 }
+
+function RenderShareTitle ({ title, icon, width = '100%' }) {
+    return (
+        <View style={[css.flex, css.js, { width }]}>
+            <Image source={icon} style={{ width: 20, height: 20, marginRight: 10 }}/>
+            <Text style={styles.pupTitleText}>{title || '-'}</Text>
+        </View>
+    );
+}
+
 const styles = StyleSheet.create({
+    btn: {
+        alignItems: 'center',
+        backgroundColor: 'rgba(255,62,0,1)',
+        height: 45,
+        justifyContent: 'center',
+        width
+    },
     formAllWrap: {
         backgroundColor: '#fff',
         paddingHorizontal: 15,
@@ -101,5 +123,7 @@ const styles = StyleSheet.create({
         color: '#000',
         fontSize: 16,
         lineHeight: 45
-    },
+    }
 });
+
+export default PupilSetPage;
