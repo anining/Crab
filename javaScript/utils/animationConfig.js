8a51a795104a5edade4a9294c2977a7e603237d8
lyk@prismslight.com <530366891@qq.com>
2020-07-07 12:32:24 +0800

    主页Lottie动画时间

--- javaScript/utils/animationConfig.js
+++ javaScript/utils/animationConfig.js
@@ -1,4 +1,4 @@
 export const HomeDelayMonitorTime = 1500;// 主页延迟监听时间
 export const HomeStartAnimationTime = 2000;// 主页开始动画时间
-export const HomeLottieStartTime = 300;// 主页Lottie动画时间
+export const HomeLottieStartTime = 800;// 主页Lottie动画时间
 export const DelayGetDomeTime = 1000;// 延迟获取dom元素位置时间 - 因为有翻页动画
