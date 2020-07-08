18686f90b399e88fd3a77477a27892d6685358ec
lyk@prismslight.com <530366891@qq.com>
2020-07-08 19:53:17 +0800

    动画时间修复

--- javaScript/utils/animationConfig.js
+++ javaScript/utils/animationConfig.js
@@ -1,4 +1,4 @@
 export const HomeDelayMonitorTime = 1000;// 主页延迟监听时间
-export const HomeStartAnimationTime = 1300;// 主页开始动画时间
-export const HomeLottieStartTime = 600;// 主页Lottie动画时间
-export const DelayGetDomeTime = 1000;// 延迟获取dom元素位置时间 - 因为有翻页动画
+export const HomeStartAnimationTime = 1100;// 主页开始动画时间
+export const HomeLottieStartTime = 500;// 主页Lottie动画时间
+export const DelayGetDomeTime = 900;// 延迟获取dom元素位置时间 - 因为有翻页动画
