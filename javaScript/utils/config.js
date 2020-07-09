1a71feded82d164163e4a6f339f4d286def2baa4
lyk@prismslight.com <530366891@qq.com>
2020-07-09 14:07:08 +0800

    hardwareAccelerated="true"

--- javaScript/utils/config.js
+++ javaScript/utils/config.js
@@ -1,5 +1,5 @@
 export const DEVELOPER = 'Staging';
-export const CONSOLE_LOG = true; // true 代表开启console
+export const CONSOLE_LOG = false; // true 代表开启console
 export const UA_ID = 10011;
 export const VERSION_CODE = 100;
 export const API_URL = DEVELOPER === 'Staging' ? 'https://dev-realman.librags.com/api' : 'https://dev-realman.librags.com/api';
