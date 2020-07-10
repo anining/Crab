ef9b827cfde72953f833302cdcd4b9d8efcae82d
Lutz <lutz.l.burning@gmail.com>
2020-07-10 17:12:59 +0800

    新手福利

--- javaScript/view/tabView/AnswerPage.js
+++ javaScript/view/tabView/AnswerPage.js
@@ -120,7 +120,7 @@ function AnswerPage () {
             if (!ret.error) {
                 const localData = ret.data.map(task => {
                     const {add_balance, task_type} = task;
-                    balances[task_type] = (balances[task_type] || 0) + add_balance;
+                    balances[task_type] = ((balances[task_type] || 0)*100 + add_balance*100)/100;
                     return task;
                 });
                 localData.forEach(item => {
@@ -130,7 +130,7 @@ function AnswerPage () {
                     }
                     if (!types[task_type]) {
                         types[task_type] = true;
-                        localUserData.push(Object.assign(item, {balance: balances[task_type]}));
+                        localUserData.push(Object.assign(item, {add_balance: balances[task_type]}));
                     }
                 });
                 setNewUser(localUserData.map(item=>{
@@ -297,7 +297,7 @@ function RenderNewList ({ list = [], _newUserTask }) {
     const view = [];
 
     list.forEach((item, index) => {
-        const { new_user_task_id,task_type,add_balance,is_finish} = item;
+        const { new_user_task_id,task_type,add_balance} = item;
         view.push(
             <View style={[styles.answerItemWrap, css.flex, css.sp, { borderBottomWidth: index + 1 >= list.length ? 0 : 1 }]} key={new_user_task_id}>
                 <View style={[css.flex, styles.aiwLeft, css.js]}>
@@ -319,7 +319,12 @@ function RenderNewList ({ list = [], _newUserTask }) {
 }
 
 function RenderNewBtn ({ item, _newUserTask }) {
+    const [local,setLocal] = useState(false);
     const {  is_finish, task_type, new_user_task_id, add_balance } = item;
+
+    useEffect(()=>{
+        asyncStorage.getItem(`NEW_USER_TASK_TYPE1${user_id.get()}`).then(r=>setLocal(r))
+    },[]);
 
     function getReward () {
         getNewUserTask(new_user_task_id).then(r => {
@@ -339,8 +344,7 @@ function RenderNewBtn ({ item, _newUserTask }) {
     }
 
     const view = U.mapValue(id=>{
-        const local1 = asyncStorage.getItem(`NEW_USER_TASK_TYPE1${user_id.get()}`);
-        const localArray = [false, local1, id, false];
+        const localArray = [false, local, id, false];
         const status = is_finish ? 3 : localArray[task_type] ? 2 : 5;
         const btnText = is_finish ? '已完成' : localArray[task_type] ? '领取奖励' : '去完成'
         const path = NEW_USER_TASK_TYPE[task_type].path;
