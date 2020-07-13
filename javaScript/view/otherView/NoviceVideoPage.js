a96ff4ca6ba14c3543c6f60f08c8f64b5f072b52
Lutz <lutz.l.burning@gmail.com>
2020-07-13 14:28:09 +0800

    观看完新手视频之后，按钮还是“去完成”不能领取奖励问题

--- javaScript/view/otherView/NoviceVideoPage.js
+++ javaScript/view/otherView/NoviceVideoPage.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import {
-    ScrollView,
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    Dimensions,
-    TouchableOpacity,
-    DeviceEventEmitter,
-} from 'react-native';
+import { StyleSheet, Text, View, DeviceEventEmitter } from 'react-native';
 import Video from 'react-native-video';
 import { NOVICE_VIDEO } from '../../utils/data';
 import loadingJson from '../../lottie/loading';
 import LottieView from 'lottie-react-native';
 import { css } from '../../assets/style/css';
 import { _if } from '../../utils/util';
-import { bindData, getPath } from '../../global/global';
+import { bindData } from '../../global/global';
 import { N } from '../../utils/router';
-import asyncStorage from '../../utils/asyncStorage';
+import { setter } from '../../utils/store';
+
 export default class NoviceVideoPage extends Component {
-    // eslint-disable-next-line no-useless-constructor
     constructor (props) {
         super(props);
         this.state = {
@@ -30,7 +21,7 @@ export default class NoviceVideoPage extends Component {
 
     async _end () {
         try {
-            asyncStorage.setItem(`NEW_USER_TASK_TYPE1${getPath(['user_id'], this.state.user, 0)}`, 'true');
+            setter([['new_user_video', true]]);
             DeviceEventEmitter.emit('reloadAnswer');
             N.goBack();
         } catch (e) {
@@ -45,31 +36,33 @@ export default class NoviceVideoPage extends Component {
     }
 
     render () {
-        return <View style={[{ flex: 1 }]}>
-            {_if(!this.state.play, res => <View style={[css.flex, css.pr, css.fw,]}>
-                <LottieView
-                    style={[{ width: 200 }]}
-                    source={loadingJson}
-                    imageAssetsFolder={'loading'}
-                    autoPlay={true}
-                    loop={true}
-                />
-                <Text style={styles.loadingText}>视频正在加载中，请耐心等候...</Text>
-            </View>)}
-            <Video source={{ uri: NOVICE_VIDEO }}
-                ref={(ref) => { this.player = ref; }}
-                controls = {true}
-                disableFocus = {true}
-                posterResizeMode={'center'}
-                resizeMode={'contain'}
-                onLoad={() => {
-                    this._onload();
-                }}
-                onEnd={async () => {
-                    await this._end();
-                }}
-                style={styles.backgroundVideo} />
-        </View>;
+        return (
+            <View style={{ flex: 1 }}>
+                {_if(!this.state.play, res => <View style={[css.flex, css.pr, css.fw]}>
+                    <LottieView
+                        style={[{ width: 200 }]}
+                        source={loadingJson}
+                        imageAssetsFolder={'loading'}
+                        autoPlay={true}
+                        loop={true}
+                    />
+                    <Text style={styles.loadingText}>视频正在加载中，请耐心等候...</Text>
+                </View>)}
+                <Video source={{ uri: NOVICE_VIDEO }}
+                    ref={(ref) => { this.player = ref; }}
+                    controls = {true}
+                    disableFocus = {true}
+                    posterResizeMode={'center'}
+                    resizeMode={'contain'}
+                    onLoad={() => {
+                        this._onload();
+                    }}
+                    onEnd={async () => {
+                        await this._end();
+                    }}
+                    style={styles.backgroundVideo} />
+            </View>
+        );
     }
 }
 
