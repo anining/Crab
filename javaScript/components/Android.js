68d6915e47546677ffa192865756de3ccf551fce
Lutz <lutz.l.burning@gmail.com>
2020-07-17 10:53:09 +0800

    IOS获取渠道

--- javaScript/components/Android.js
+++ javaScript/components/Android.js
@@ -1,8 +1,14 @@
-import { NativeModules } from 'react-native';
+import { Platform, NativeModules } from 'react-native';
 
 let android = {};
 try {
-    NativeModules.TransmitModule && (android = NativeModules.TransmitModule);
+    if (Platform.OS === 'ios') {
+        android.promiseGetChannel = () => new Promise((resolve, reject) => {
+            resolve({ channel: 'ios_crab' });
+        });
+    } else {
+        NativeModules.TransmitModule && (android = NativeModules.TransmitModule);
+    }
 } catch (e) {
     console.log(e);
 }
